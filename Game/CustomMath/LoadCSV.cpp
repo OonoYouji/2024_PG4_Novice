424ad6db06f737e5a17427ea7201af46bf154d72
OonoYouji <k023g0007@g.neec.ac.jp>
2025-01-20 10:20:13 +0900

    csvファイルの読み込みを作成

--- Game/CustomMath/LoadCSV.cpp
+++ Game/CustomMath/LoadCSV.cpp
@@ -1,9 +1,36 @@
 #include "LoadCSV.h"
 
+/// std
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
 std::vector<std::vector<int>> LoadCSV(const std::string& _filePath) {
 	std::vector<std::vector<int>> csvData;
 
+	/// ファイルを開く
+	std::ifstream file(_filePath);
+	if (!file.is_open()) {
+		std::cerr << "ファイルを開くことができません: " << _filePath << std::endl;
+		return csvData;
+	}
 
+	/// ファイル内のデータをcsvDataに格納する
+	std::string line;
+	while (std::getline(file, line)) {
+		
+		std::vector<int> row;
+		std::stringstream lineStream(line);
+		std::string cell;
 
+		while (std::getline(lineStream, cell, ',')) {
+			row.push_back(std::stoi(cell));
+		}
+		
+		csvData.push_back(row);
+	}
+
+	file.close();
 	return csvData;
 }
--- Game/Scenes/GameScene.cpp
+++ Game/Scenes/GameScene.cpp
@@ -4,15 +4,15 @@
 #include <cassert>
 
 /// user
-#include "ClearScene.h"
+#include "../CustomMath/LoadCSV.h"
 
 GameScene::GameScene() {}
 GameScene::~GameScene() {}
 
 void GameScene::Initialize() {
 
-
 	/// multi threadでの読み込みを行う
+	std::vector<std::vector<int>> csvData = LoadCSV("./Resources/CSVData.csv");
 
 	
 }
