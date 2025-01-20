a78211bca6236340859936c2e0c3b172e8465713
OonoYouji <k023g0007@g.neec.ac.jp>
2025-01-20 09:59:37 +0900

    いらないファイルの削除

--- Game/Scenes/GameScene.h
+++ Game/Scenes/GameScene.h
@@ -6,7 +6,6 @@
 #include <memory>
 
 #include "IScene/IScene.h"
-#include "../Entities/IEntity/IEntity.h"
 
 class GameScene final : public IScene {
 public:
@@ -18,16 +17,8 @@ public:
 	void Update()     override;
 	void Draw()       override;
 
-	void CollisionALL();
-
 private:
 
-	class Player* player_ = nullptr;
-	class Enemy*  enemy_  = nullptr;
-
-	std::vector<std::unique_ptr<IEntity>> entities_;
-
-	std::list<IEntity*> collisionEntities_;
 
 
 };
